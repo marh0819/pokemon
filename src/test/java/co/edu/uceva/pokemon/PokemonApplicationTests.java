588c8ac32819f9abfdfc2f2c7f7d768fb18182f4
marh0819 <miguel.rivera01@uceva.edu.co>
2024-10-09 12:26:39 -0500

    first commit

---
+++ src/test/java/co/edu/uceva/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
