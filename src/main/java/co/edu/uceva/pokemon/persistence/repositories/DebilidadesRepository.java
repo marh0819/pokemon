3517f28bc8f2ccad6db65c43b3aec75a36d2eeef
marh0819 <miguel.rivera01@uceva.edu.co>
2024-10-17 18:45:30 -0500

    configuracion del pom

--- src/main/java/co/edu/uceva/pokemon/persistence/repositories/DebilidadesRepository.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uceva.pokemon.persistence.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import co.edu.uceva.pokemon.persistence.entities.DebilidadesEntity;
-import co.edu.uceva.pokemon.persistence.entities.DebilidadesId;
-
-public interface DebilidadesRepository extends JpaRepository<DebilidadesEntity, DebilidadesId> {
-}
